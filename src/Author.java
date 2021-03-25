e80730f88e15b321fb8a080add27c7f32fc0af51
Никита <grand.marchello@gmail.com>
2021-03-25 22:48:20 +0400

    Решение задания 6

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,35 @@
 public class Author {
+    private String name,email;
+    private char gender;
+
+
+
+    String ToString() {
+        name = getName();
+        email = getEmail();
+        gender = getGender();
+        return (name +" (" + gender + ") " + email);
+    }
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,46 @@
+import java.util.Random;
+
 public class Book {
+    private String name = "Legendary Book",author;
+    private double price;
+    private int qty;
+    final Random random = new Random();
+
+    void ToString(){
+        Author authorSecondClass = new Author("Александр Пушкин","AlehandroPushka@gmail.com", 'm');
+        name = getName();
+        author = authorSecondClass.getName();
+        setPrice((double)random.nextInt(100));
+        setQty(random.nextInt(10));
+        System.out.println("\"" + getName() + "\" от " + authorSecondClass.ToString() + "  \n\\\\Дополнительно сделал:"
+                + "\nСтоимостью: " + getPrice() + " $\nВ количестве: " + getQty() + " штук.");
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+
+
+
 }
+
